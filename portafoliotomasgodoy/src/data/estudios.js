f92cb7a028c0afb74b16101614f237d5326fe48f
Tomás Godoy <tomas.godoy.litio@hotmail.com>
2026-03-26 15:17:33 -0300

    Actualizacion de cursados

--- portafoliotomasgodoy/src/data/estudios.js
+++ portafoliotomasgodoy/src/data/estudios.js
@@ -46,14 +46,10 @@ export const estudiosfinalizados = [
         titulo: 'Testing',
         institucion: 'Coderhouse',
         fecha: '5 de septiembre año 2024 - 12 de diciembre año 2024',
-    },
-
-]
-
-export const estudiosproximos = [
+    },    
     {
         id: 210,
-        titulo: 'Data Analitycs (Cursando)',
+        titulo: 'Data Analitycs',
         institucion: 'Coderhouse',
         fecha: '25 de agosto año 2025 - 28 de octubre año 2025',
     },
@@ -63,28 +59,37 @@ export const estudiosproximos = [
         institucion: 'Coderhouse',
         fecha: '28 de octubre año 2025 - 6 de enero año 2026',
     },
-    {
+        {
         id: 212,
         titulo: 'Data Science "1"',
         institucion: 'Coderhouse',
         fecha: '3 de noviembre año 2025 - 2 de febrero año 2026',
     },
+]
+
+export const estudiosproximos = [
     {
         id: 213,
         titulo: 'Data Science "2"',
         institucion: 'Coderhouse',
-        fecha: '26 de febrero año 2026 - 18 de junio año 2026',
+        fecha: '12 de diciembre año 2026',
     },
     {
         id: 214,
         titulo: 'Data Science "3"',
         institucion: 'Coderhouse',
-        fecha: '11 de julio año 2026 - 1 de agosto año 2026',
+        fecha: '19 de septiembre año 2027',
     },
     {
         id: 215,
         titulo: 'Tecnicatura programación',
+        institucion: 'Universidad Tecnológica Nacional (UTN2 - Rosario)',
+        fecha: 'marzo año 2026 a la actualidad',
+    },
+    {
+        id: 216,
+        titulo: 'Ingeniera en sistemas',
         institucion: 'Universidad Tecnológica Nacional (UTN - Rosario)',
-        fecha: 'marzo año 2026 - diciembre año 2027',
+        fecha: 'marzo año 2026 a la actualidad',
     },
 ]
